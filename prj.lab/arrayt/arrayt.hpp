ad0e73a2ea87acaa23b718aa915a46616d0dcc02
dvpsun <dvpsun@gmail.com>
2024-12-03 08:35:39 +0300

    upd: arrayd arrayt

--- prj.lab/arrayt/arrayt.hpp
+++ prj.lab/arrayt/arrayt.hpp
@@ -27,8 +27,8 @@ public:
   void Resize(const std::ptrdiff_t size) { size_ = size; }
   
   //! \param idx - индекс  элемента, 0 <= idx < Size()
-  [[nodiscard]] double& operator[](const std::ptrdiff_t idx) { (idx);  return *data_; }
-  [[nodiscard]] double operator[](const std::ptrdiff_t idx) const { (idx);  return *data_; }
+  [[nodiscard]] T& operator[](const std::ptrdiff_t idx);
+  [[nodiscard]] const T& operator[](const std::ptrdiff_t idx) const;
 
   //! \param idx - индекс вставляемого элемента, 0 <= idx <= size 
   void Insert(const std::ptrdiff_t idx, const double val) { (idx); (val); }
@@ -39,7 +39,7 @@ public:
 private:
   std::ptrdiff_t capacity_ = 0;  //!< размер буффера
   std::ptrdiff_t size_ = 0;      //!< число элементов в массиве
-  std::unique_ptr<T> data_;      //!< буффер
+  std::unique_ptr<T[]> data_;      //!< буффер
 };
 
 
@@ -47,7 +47,7 @@ template<class T>
 ArrayT<T>::ArrayT(const ArrayT<T>& src)
   : capacity_(src.size_)
   , size_(capacity_) {
-  data_ = std::make_unique<T[]>(capacity_);
+  data_.reset(new T[capacity_]{T()});
   std::copy(src.data_.get(), src.data_.get() + size_, data_.get());
 }
 
@@ -58,7 +58,7 @@ ArrayT<T>::ArrayT(const std::ptrdiff_t size)
   if (size_ <= 0) {
     throw std::invalid_argument("ArrayD::ArrayD - non positive size");
   }
-  data_ = std::make_unique<T[]>(size);
+  data_.reset(new T[size]{T()});
 }
 
 template<class T>
@@ -68,6 +68,23 @@ ArrayT<T>& ArrayT<T>::operator=(const ArrayT<T>& rhs) {
     std::copy(rhs.data_.get(), rhs.data_.get() + size_, data_.get());
   }
   return *this;
+}
+
+
+template<class T>
+T& ArrayT<T>::operator[](const std::ptrdiff_t idx) {
+  if (idx < 0 || size_ <= idx) {
+    throw std::invalid_argument("ArrayD::operator[] - invalid argument");
+  }
+  return *(data_.get() + idx);
+}
+
+template<class T>
+const T& ArrayT<T>::operator[](const std::ptrdiff_t idx) const {
+  if (idx < 0 || size_ <= idx) {
+    throw std::invalid_argument("ArrayD::operator[] - invalid argument");
+  }
+  return *(data_.get() + idx);
 }
 
 #endif // !ArrayT_ArrayT_HPP_20241202
